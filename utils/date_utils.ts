bf36fca8001fe715c4d2e0819988d2e01ffbf66e
tomimandalap <tommymandalaputra@gmail.com>
2025-02-06 14:38:21 +0700

    feat: added func timestampFormat

--- utils/date_utils.ts
+++ utils/date_utils.ts
@@ -1,12 +1,22 @@
 export enum DATE_FORMAT {
   DATE = 'DD MMM YYYY',
-  DATETIME = 'DD MMM YYYY, HH:mm',
-  TIME = 'HH:mm a',
+  DATETIME = 'DD MMM YYYY, HH:mm A',
+  TIME = 'HH:mm A',
 }
 
 export function dateFormat(date?: string, format: DATE_FORMAT | string = DATE_FORMAT.DATETIME) {
   if (!date)
     return undefined
+
+  return useDateFormat(date, format).value
+}
+
+export function timestampFormat(timestamp: number, format: DATE_FORMAT | string = DATE_FORMAT.DATETIME) {
+  if (!timestamp)
+    return undefined
+
+  // Convert timestamp (seconds) to Date
+  const date = new Date(timestamp * 1000)
 
   return useDateFormat(date, format).value
 }
